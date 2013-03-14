3f7ebf49c841fa4e212fd13b82608a3bae8f209f
Mike Pennisi <mike@mikepennisi.com>
2013-03-14 19:01:59 -0400

    Improve Public Peer File validation tests
    Use an externally-loaded valid document and delete individual parts,
    testing validation logic over isolated errors.

--- test/public/tests/peer-file-public.js
+++ test/public/tests/peer-file-public.js
@@ -1,10 +1,11 @@
 /* global suite, test, assert, beforeEach, before, after */
-define(['opjs/stack/peer-file-public'], function (Pfp) {
+define(['opjs/stack/peer-file-public', 'text!/data/public-peer-file.json'],
+  function (Pfp, pblcJSON) {
   'use strict';
 
   suite('PeerFilePublic', function () {
     suite('#load', function () {
-      var pfp;
+      var pfp, doc;
       var console$error;
 
       // Silence error reporting
@@ -17,49 +18,42 @@ define(['opjs/stack/peer-file-public'], function (Pfp) {
       });
 
       beforeEach(function () {
+        doc = JSON.parse(pblcJSON).peer;
         pfp = new Pfp();
       });
 
-      test('Rejects malformed documents', function () {
-        var doc, sectionBundle, sectionA, saltBundle, saltSig;
-
+      test('Rejects documents without a section A', function () {
+        doc.sectionBundle.shift();
         assert.isFalse(pfp.load(doc));
+      });
 
-        doc = {};
+      test('Rejects documents without section A content', function () {
+        delete doc.sectionBundle[0].section;
         assert.isFalse(pfp.load(doc));
+      });
 
-        sectionBundle = doc.sectionBundle = [];
+      test('Rejects documents without a salt bundle', function () {
+        delete doc.sectionBundle[0].section.saltBundle;
         assert.isFalse(pfp.load(doc));
+      });
 
-        sectionBundle.push({});
+      test('Rejects documents without a salt bundle signature', function () {
+        delete doc.sectionBundle[0].section.saltBundle.signature;
         assert.isFalse(pfp.load(doc));
+      });
 
-        sectionA = {};
-        sectionBundle.push(sectionA);
+      test('Rejects documents without a salt signature key', function () {
+        delete doc.sectionBundle[0].section.saltBundle.signature.key;
         assert.isFalse(pfp.load(doc));
+      });
 
-        sectionA.section = {};
+      test('Rejects documents without a salt signature key domain', function () {
+        delete doc.sectionBundle[0].section.saltBundle.signature.key.domain;
         assert.isFalse(pfp.load(doc));
+      });
 
-        sectionA.section.$id = 'A';
-        assert.isFalse(pfp.load(doc));
-
-        sectionA.signature = {};
-        assert.isFalse(pfp.load(doc));
-
-        sectionA.section.cipher = 'sha256/aes256';
-        assert.isFalse(pfp.load(doc));
-
-        saltBundle = sectionA.section.saltBundle = {};
-        assert.isFalse(pfp.load(doc));
-
-        saltSig = saltBundle.signature = {};
-        assert.isFalse(pfp.load(doc));
-
-        saltSig.key = {};
-        assert.isFalse(pfp.load(doc));
-
-        saltSig.key.domain = '@#%#@$invalid domain///';
+      test('Rejects documents with an invalid salt signature key domain', function () {
+        doc.sectionBundle[0].section.saltBundle.signature.key.domain = '@#%#@$invalid domain///';
         assert.isFalse(pfp.load(doc));
       });
     });
